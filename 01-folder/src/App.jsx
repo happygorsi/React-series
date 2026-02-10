052d1189d190d66bc8adadeab699abd1ef68b6d8
learning-git <gorsihappy152@gmail.com>
2026-02-10 17:12:42 +0530

    first project

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+
+function App() {
+
+  return (
+    <>
+      <div class="name">
+        <h1>HAPPY GORSI</h1>
+      </div>
+      <div class="age">
+        <h2>22 years</h2>
+      </div>
+    </>
+
+  )
+
+}
+
+export default App
