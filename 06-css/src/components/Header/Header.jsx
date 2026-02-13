94d6b86e853fcd4634b9f3e1efdf34394f5e8026
learning-git <gorsihappy152@gmail.com>
2026-02-13 19:25:37 +0530

    different css different component

---
+++ 06-css/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+const Header = () => {
+    return (
+            <div className={styles.header}>
+                <h3>Happy Gorsi</h3>
+                 <button> Insights</button>
+            </div>
+    )
+}
+
+export default Header
